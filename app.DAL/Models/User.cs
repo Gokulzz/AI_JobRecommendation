0788a7dcdd9f9c079db6b2553628699c0f014470
Gokulzz <gokulkhatri2020@gmail.com>
2024-08-27 15:33:28 -0400

    Add project files.

---
+++ app.DAL/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.DAL.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid userId= Guid.NewGuid();
+        public string userName { get; set; }  
+        public string Email { get; set; }
+        public byte[] passwordHash { get; set; }   
+        public byte[] passwordSalt { get; set; }   
+        public string verfificationToken { get; set; }  
+        public DateTime? verfiedAt { get; set; }    
+        
+        
+    }
+}
---
+++ app.DAL/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.DAL.Models
+{
+    public class UserProfile
+    {
+        public Guid profileId= Guid.NewGuid();
+        public Guid userId { get; set; }
+    }
+}
